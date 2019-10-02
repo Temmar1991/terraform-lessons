ff7b563dce0577738ef20b34afad8696f9ed4ab9
Artem Melnyk <temmar1991@gmail.com>
2019-10-02 12:33:01 +0300

    adding datasource accout_id

--- aws/Lesson-8/main.tf
+++ aws/Lesson-8/main.tf
@@ -4,9 +4,19 @@ provider "aws" {
 
 data "aws_availability_zones" "working" {}
 
+data "aws_caller_identity" "current" {
+  
+}
+
+
 
 output "data_aws_availabitity_zones" {
   value = data.aws_availability_zones.working.names[1]
 }
+
+output "data_aws_caller_identity" {
+  value = data.aws_caller_identity.current.account_id
+}
+
 
 
