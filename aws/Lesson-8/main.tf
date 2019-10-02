ef7706ed099ae0b8c9c58dcc0492bd491c6c1fcb
Artem Melnyk <temmar1991@gmail.com>
2019-10-02 13:00:17 +0300

    creating two subnet in prod vpc

--- aws/Lesson-8/main.tf
+++ aws/Lesson-8/main.tf
@@ -18,6 +18,33 @@ data "aws_vpc" "prod_vpc" {
   }
 }
 
+resource "aws_subnet" "prod_subnet_1" {
+  vpc_id = "${data.aws_vpc.prod_vpc.id}"
+  availability_zone = "${data.aws_availability_zones.working.names[0]}"
+  cidr_block = "10.20.1.0/24"
+
+  tags = {
+      Name = "subnet-1 in ${data.aws_availability_zones.working.names[0]}"
+      Account = "Subnet in Account ${data.aws_caller_identity.current.account_id}"
+      Region = "${ data.aws_region.current.description}"
+  }
+}
+
+resource "aws_subnet" "prod_subnet_2" {
+  vpc_id = "${data.aws_vpc.prod_vpc.id}"
+  availability_zone = "${data.aws_availability_zones.working.names[1]}"
+  cidr_block = "10.20.1.0/24"
+
+  tags = {
+      Name = "subnet-2 in ${data.aws_availability_zones.working.names[1]}"
+      Account = "Subnet in Account ${data.aws_caller_identity.current.account_id}"
+      Region = "${ data.aws_region.current.description}"
+  }
+}
+
+
+
+
 output "prod_vpc_id" {
   value = "${data.aws_vpc.prod_vpc.id}"
 }
