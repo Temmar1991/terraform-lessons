f748783fc5e091fa6af6a12c0c15eb8c6ccc0400
Artem Melnyk <temmar1991@gmail.com>
2019-10-09 22:41:19 +0300

    fix

--- aws/Lesson-9/main.tf
+++ aws/Lesson-9/main.tf
@@ -15,10 +15,10 @@ data "aws_ami" "latest_ubuntu" {
 
 
 output "latest_ubuntu_ami_id" {
-  value = "data.aws_ami.latest_ubuntu.id"
+  value = "${data.aws_ami.latest_ubuntu.id}"
 }
 
 output "latest_ubuntu_ami_name" {
-  value = "data.aws_ami.latest_ubuntu.name"
+  value = "${data.aws_ami.latest_ubuntu.name}"
 }
 
