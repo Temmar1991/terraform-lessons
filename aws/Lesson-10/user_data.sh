bf09f5a17ec583557407d32c3a45949af767c2d4
Artem Melnyk <temmar1991@gmail.com>
2019-10-12 00:37:02 +0300

    Adding HA web server

---
+++ aws/Lesson-10/user_data.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+yum -y update
+yum -y install httpd
+myip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo <<EOF > /var/www/html/index.html
+<html>
+<body bgcolor="blue"
+<h2><font color="gold">Build by Power of Terraform <font color="red">0.12</font></h2><br><p>
+<font color="green>"Server  PrivateIP: <font coor="aqua">$myip<br><br>
+
+<font color="magenta">
+<b>Version 1.0</b>
+</body>
+</html>
+EOF
+
+sudo service httpd start
+chkconfig httd on   
