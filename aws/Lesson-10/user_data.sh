e40d50f8a2d3a63bd066d4532a1c3d68597d71d4
Artem Melnyk <temmar1991@gmail.com>
2019-10-25 23:01:15 +0300

    fix script

--- aws/Lesson-10/user_data.sh
+++ aws/Lesson-10/user_data.sh
@@ -4,7 +4,7 @@ yum -y install httpd
 myip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 echo <<EOF > /var/www/html/index.html
 <html>
-<body bgcolor="blue"
+<body bgcolor="blue">
 <h2><font color="gold">Build by Power of Terraform <font color="red">0.12</font></h2><br><p>
 <font color="green>"Server  PrivateIP: <font coor="aqua">$myip<br><br>
 
