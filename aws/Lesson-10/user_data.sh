b7c700b215069f6b9069b7704f20ca2115af0ea1
Artem Melnyk <temmar1991@gmail.com>
2019-10-25 23:22:01 +0300

    fix script

--- aws/Lesson-10/user_data.sh
+++ aws/Lesson-10/user_data.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+
 yum -y update
 yum -y install httpd
-myip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-echo <<EOF > /var/www/html/index.html
+
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+
+cat <<EOF > /var/www/html/index.html
 <html>
 <body bgcolor="blue">
 <h2><font color="gold">Build by Power of Terraform <font color="red">0.12</font></h2><br><p>
