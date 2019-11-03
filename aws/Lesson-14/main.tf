47400d351705625c149bf9db2a5b17d14e637918
Artem Melnyk <temmar1991@gmail.com>
2019-11-03 23:21:45 +0200

    adding commands ping anfd python script

--- aws/Lesson-14/main.tf
+++ aws/Lesson-14/main.tf
@@ -7,3 +7,18 @@ resource "null_resource" "command1" {
       command = "echo Terraform START: $(date) >> log.txt"
   }
 }
+
+resource "null_resourcw" "command2" {
+  provisioner "local-exec" {
+      command = "ping -c 5 www.google.com"
+  }
+
+  depends_on = [null_resource.command1]
+}
+
+resource "null_resourcw" "command3" {
+  provisioner "local-exec" {
+      command = "print('Hello world!')"
+      interpreter = ["python" , "-c"]
+  }
+}
