e3100a0165de2d7cd4b788695fe180f9d411efd3
Artem Melnyk <temmar1991@gmail.com>
2019-11-03 23:41:38 +0200

    adding exec command as part of resource creation

--- aws/Lesson-14/main.tf
+++ aws/Lesson-14/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+data "aws_ami" "latest_amazon_linux" {
+  owners = ["amazon"]
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-*"]
+  }
+  
+}
+
 resource "null_resource" "command1" {
   provisioner "local-exec" {
       command = "echo Terraform START: $(date) >> log.txt"
@@ -31,5 +41,22 @@ resource "null_resource" "command4" {
           NAME2 = "Petya"
           NAME3 = "Kolya"
       }
+  }
+}
+
+resource "aws_instance" "myserver" {
+  ami = "${data.aws_ami.latest_amazon_linux.id}"
+  instance_type = "t3.micro"
+  provisioner "local-exec" {
+      command = "echo Hello from AWS Instance Creations!"
+  }
+}
+
+
+resource "null_resource" "command6" {
+  provisioner "local-exec" {
+      command = "echo Terraform END: $(date) >> log.txt"
+
+      depends_on = [null_resource.command1, null_resource.command2, null_resource.command3, null_resource.command4, aws_instance.myserver ]
   }
 }
