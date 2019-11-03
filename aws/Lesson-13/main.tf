e9f7387d38a1140d493dfefe4f25a909193ea5cd
Artem Melnyk <temmar1991@gmail.com>
2019-11-03 22:56:42 +0200

    adding new complicated locals

--- aws/Lesson-13/main.tf
+++ aws/Lesson-13/main.tf
@@ -7,6 +7,23 @@ locals {
   project_owner = "${var.owner} owner of ${var.project_name}"
 }
 
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {
+  
+}
+
+
+
+locals {
+  country = "Ukraine"
+  city = "Kyiv"
+  az_list = join(",", data.aws_availability_zones.available.names)
+  region = data.aws_region.current.description
+  location = "In ${local.az_list} there are AZ: ${local.az_list}" 
+}
+
+
 
 resource "aws_eip" "my_static_ip" {
     tags = {
@@ -14,6 +31,10 @@ resource "aws_eip" "my_static_ip" {
         Owner = "var.owner"
         Project = "${local.full_project_name}"
         proj_owner = "${local.project_owner}"
+        city = "${local.city}"
+        region_azs = "${local.az_list}"
+        location = "${local.location}" 
+
     }
   
 }
