fe989d4b5debb85492786768add444143464eada
Artem Melnyk <temmar1991@gmail.com>
2019-11-03 23:15:36 +0200

    fix 2 lesson-14

--- aws/Lesson-14/main.tf
+++ aws/Lesson-14/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "null_resourse" "command1" {
+resource "null_resource" "command1" {
   provisioner "local-exec" {
       command = "echo Terraform START: $(date) >> log.txt"
   }
