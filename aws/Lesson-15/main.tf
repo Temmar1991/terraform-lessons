430d67d00429bff434ad050a789ad8ffb7ed4b5d
Artem Melnyk <temmar1991@gmail.com>
2019-11-20 23:54:58 +0200

    adding tags with conditions

--- aws/Lesson-15/main.tf
+++ aws/Lesson-15/main.tf
@@ -7,6 +7,17 @@ variable "env" {
   default = "prod"
 }
 
+variable "prod_owner" {
+    default = "Artem Melnyk"
+  
+}
+
+variable "noprod_owner" {
+  default = "Dyadya Vasya"
+}
+
+
+
 data "aws_ami" "amazon_ami" {
   most_recent = true
   owners = ["amazon"]
@@ -20,5 +31,11 @@ data "aws_ami" "amazon_ami" {
 resource "aws_instance" "my_webserver1" {
   ami = "${data.aws_ami.amazon_ami.id}"
   instance_type = (var.env == "prod" ? "t2.large" : "t2.micro")
+
+  tags = {
+      Name = "${var.env}-server"
+      Owner = ${var.env} == "prod" ? "${var.prod_owner}" : "pa${var.noprod_owner}"
+
+  }
 }
 
