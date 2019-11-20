7eee912f31a6107d506b4dc57add7785a71f9252
Artem Melnyk <temmar1991@gmail.com>
2019-11-21 00:26:39 +0200

    adding more cond

--- aws/Lesson-15/main.tf
+++ aws/Lesson-15/main.tf
@@ -26,6 +26,16 @@ variable "eec2_size" {
 }
 
 
+variable "allow_port_list" {
+    default = {
+        "prod" = ["80", "443"]
+        "dev" = ["80", "443", "8080", "22"]
+
+    }
+  
+}
+
+
 
 data "aws_ami" "amazon_ami" {
   most_recent = true
@@ -39,7 +49,8 @@ data "aws_ami" "amazon_ami" {
 
 resource "aws_instance" "my_webserver1" {
   ami = "${data.aws_ami.amazon_ami.id}"
-  instance_type = (var.env == "prod" ? "t2.large" : "t2.micro")
+#   instance_type = (var.env == "prod" ? "t2.large" : "t2.micro")
+  instance_type = "${var.env}" == "prod" ? "${var.eec2_size["prod"]}" : "${var.eec2_size["dev"}"
 
   tags = {
       Name = "${var.env}-server"
@@ -69,3 +80,33 @@ resource "aws_instance" "my_dev_bastion" {
       Name = "Bastion Server for Dev-Sever"
   }
 }
+
+
+resource "aws_security_group" "my_webserver-dynamic" {
+  name = "WebServer Security Group"
+  description = "My First SecurityGroup"
+  
+
+  dynamic "ingress" {
+    for_each = lookup("${var.allow_port_list}", "${var.env}")
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WebServer Security Group"
+    Owner = "Artem Melnyk"
+  }
+
+} 
