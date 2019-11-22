b5d114acb76e89ba31c15d87cf9cd9daa0909d36
Artem Melnyk <temmar1991@gmail.com>
2019-11-23 01:10:06 +0200

    fix lesson-16

--- aws/Lesson-16/main.tf
+++ aws/Lesson-16/main.tf
@@ -12,7 +12,7 @@ resource "aws_iam_user" "user1" {
   name = "pushkin"
 }
 
-resource "aws_iam" "users" {
+resource "aws_iam_user" "users" {
   count = length("${var.aws_users}")  
   name = element("${var.aws_users}", count.index)
 }
