08d0cba9846f2a4da23d8750955e043c2d4cd364
Artem Melnyk <temmar1991@gmail.com>
2019-11-23 00:58:40 +0200

    generate  iam users from list

--- aws/Lesson-16/main.tf
+++ aws/Lesson-16/main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+
+variable "aws_users" {
+  description = "List of IAM Users to create"
+  default = ["vasya", "petya", "kolya","lena","masha", "misha", "vova"]
+}
+
 resource "aws_iam_user" "user1" {
   name = "pushkin"
+}
+
+resource "aws_iam" "users" {
+  count = length("${var.aws_users}")  
+  name = element("${var.aws_users}", count.index)
 }
 
 
