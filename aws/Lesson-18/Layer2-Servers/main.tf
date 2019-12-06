01ef0d01e4ffac5b43ebdcd2c86badfee6c49dbe
Artem Melnyk <temmar1991@gmail.com>
2019-12-06 23:34:59 +0200

    get vars from remote_state

--- aws/Lesson-18/Layer2-Servers/main.tf
+++ aws/Lesson-18/Layer2-Servers/main.tf
@@ -13,31 +13,38 @@ data "terraform_remote_state" "network" {
 }
 
 
-# resource "aws_security_group" "webserver" {
+resource "aws_security_group" "webserver" {
   
-#   name = "WebServer Security Group"
-#   vpc_id = 
+  name = "WebServer Security Group"
+  vpc_id = "$data.terraform_remote_state.network.outputs.vpc_id"
 
-#   ingress {
-#       from_port = 80
-#       to_port = 80
-#       protocol = "tcp"
-#       cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port = 0
-#     to_port   = 0
-#     protocol = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["${data.terraform_remote_state.network.outputs.vpc_cidr}"]
+  }
 
-#   tags = {
-#     Name = "web-server-sg"
-#     Owner = "Artem Melnyk"
-#   }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# }
+  tags = {
+    Name = "web-server-sg"
+    Owner = "Artem Melnyk"
+  }
+
+}
 
 
 output "network_details" {
