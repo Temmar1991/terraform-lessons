c5932e3e49a113fd35597c61b4d5f8fabc7548f2
Artem Melnyk <temmar1991@gmail.com>
2019-12-06 23:29:40 +0200

    adding Layer-2

---
+++ aws/Lesson-18/Layer2-Servers/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+
+data "terraform_remote_state" "network" {
+    backend = "s3"
+    config = {
+        bucket = "amelnyk-terraform-state"
+        key = "dev/network/terraform.tfstate"
+        region = "eu-central-1"
+    }
+}
+
+
+# resource "aws_security_group" "webserver" {
+  
+#   name = "WebServer Security Group"
+#   vpc_id = 
+
+#   ingress {
+#       from_port = 80
+#       to_port = 80
+#       protocol = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port = 0
+#     to_port   = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "web-server-sg"
+#     Owner = "Artem Melnyk"
+#   }
+
+# }
+
+
+output "network_details" {
+  value = "${data.terraform_remote_state.network}"
+}
+
+
+
+
